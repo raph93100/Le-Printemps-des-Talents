5f1278747d70169e4401f70908ec6a68e492e998
raph93100 <72013628+raph93100@users.noreply.github.com>
2021-07-05 11:52:59 +0200

    Sections principales
    Ajout de l'onglet billetterie
    Ajout de l'onglet exposant
    Ajout de l'onglet carte
    Ajout d'un document js

---
+++ mortar-privileged-empty-app/main.js
@@ -0,0 +1,8 @@
+var menuIcon = document.getElementById("menuIcon")
+var menu = document.getElementById("menu")
+
+menuIcon.addEventListener('click', function() {
+    menu.style.display = "block"
+});
+console.log(menu)
+
